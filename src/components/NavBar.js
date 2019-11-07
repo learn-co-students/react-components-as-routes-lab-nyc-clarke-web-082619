9a1823cc08e6f763284d5307bf6dc9c0c2ed9010
github username <github email address>
2019-11-07 16:57:35 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        // style={}
+        // activeStyle={}
+      >
+        Home
+      </NavLink>
+      <br></br>
+      <NavLink
+        to="/movies"
+        exact
+        // style={}
+        // activeStyle={}
+      >
+        Movies
+      </NavLink>
+      <br></br>
+      <NavLink
+        to="/directors"
+        exact
+        // style={}
+        // activeStyle={}
+      >
+        Directors
+      </NavLink>
+      <br></br>
+      <NavLink
+        to="/actors"
+        exact
+        // style={}
+        // activeStyle={}
+      >
+        Actors
+      </NavLink>
+      <br></br>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './containers/App';
+import React from "react";
+import { render } from "react-dom";
+import App from "./containers/App";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
-render (
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById("root"));
